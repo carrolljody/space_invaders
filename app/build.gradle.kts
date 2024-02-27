e3e201573fd64a17076e5d98373f55153a2a8ce5
carrolljody <75698201+carrolljody@users.noreply.github.com>
2024-02-27 21:47:54 +0100

    bump gradle version + android sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.facil.space_invaders"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.facil.space_invaders"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
